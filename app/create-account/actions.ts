be5777b33d7e4f501a0ce391bced65d800196b95
문태호(Mun TaeHo) <mun05170@gmail.com>
2024-05-04 23:47:06 +0900

    iron-session

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -7,6 +7,9 @@ import {
 import db from "@/lib/db";
 import { z } from "zod";
 import bcrypt from "bcrypt";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const checkUniqueUsername = async (username: string) => {
   const user = await db.user.findUnique({
@@ -89,5 +92,17 @@ export async function createAccount(prevState: any, formData: FormData) {
     });
 
     console.log(user);
+
+    const cookie = await getIronSession(cookies(), {
+      cookieName: "my-carrot",
+      password: process.env.COOKIE_PASSWORD!,
+    });
+
+    //@ts-ignore
+    cookie.id = user.id;
+
+    await cookie.save();
+
+    redirect("/profile");
   }
 }
