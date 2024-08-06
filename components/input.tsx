3fe9f169e4f2a6a95de66d6521f997504e1ebaff
Taeho <mun05170@gmail.com>
2024-08-06 10:47:06 +0900

    Feat: number wheel 이벤트 제한

--- components/input.tsx
+++ components/input.tsx
@@ -16,6 +16,7 @@ const _Input = (
         ref={ref}
         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400 px-3"
         {...rest}
+        onWheel={(e) => e.currentTarget.blur()}
       />
       {errors.map((error, index) => (
         <span key={index} className="text-red-500 font-medium">
