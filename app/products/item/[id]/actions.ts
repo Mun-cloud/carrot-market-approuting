18b5bbf1ef513a0218b3459d3d075d0fb27ff06c
Taeho <mun05170@gmail.com>
2024-08-08 18:16:15 +0900

    Feat: product 서버 액션 정리

--- app/products/item/[id]/actions.ts
+++ app/products/item/[id]/actions.ts
@@ -2,6 +2,9 @@
 
 import { unstable_cache as nextCache } from "next/cache";
 import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+import { href } from "@/lib/href";
 
 async function getProduct(id: number) {
   return await db.product.findUnique({
@@ -20,3 +23,32 @@ async function getProduct(id: number) {
 export const getCachedProduct = nextCache(getProduct, ["product-detail"], {
   tags: ["product-detail"],
 });
+
+export async function createChatRoom(formData: FormData) {
+  const productOwnerId = Number(formData.get("productOwnerId") as string);
+  const session = await getSession();
+
+  if (productOwnerId === session.id) return;
+
+  const room = await db.chatRoom.create({
+    data: {
+      users: {
+        connect: [{ id: productOwnerId }, { id: session.id }],
+      },
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  redirect(`${href.chats}/${room.id}`);
+}
+
+export async function getIsProductOwner(userId: number) {
+  const session = await getSession();
+  if (session.id) {
+    return session.id === userId;
+  }
+
+  return false;
+}
